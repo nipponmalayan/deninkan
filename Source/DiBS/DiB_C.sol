13342a2322a30a55cd97aa79600f8161e5dacc0c
Reiji Reizan <matsurinoyama@pm.me>
2020-10-08 13:04:36 +0800

    ❇Created DiW folder for deninkan web builds

---
+++ Source/DiBS/DiB_C.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: CC-BY-NC-SA-4.0
+pragma solidity ^0.5.0;
+
+contract DiB_C {
+    function createCID(uint256) public view returns (uint256 CID) {
+        return
+            CID =
+                (uint256(
+                    keccak256(
+                        abi.encodePacked(
+                            block.timestamp,
+                            block.difficulty,
+                            msg.sender
+                        )
+                    )
+                ) % 10000000) +
+                10000000;
+    }
+}
---
+++ Source/DiBS/DiB_USER.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: CC-BY-NC-SA-4.0
+pragma solidity ^0.5.0;
+
+contract DiB_USER {
+    
+    struct User {
+        
+        address id;
+        string name;
+        string dname;
+        bool exist;
+        
+    }
+    
+    mapping(string => User) public users;
+    
+    function createUser(string memory _userName, string memory _userDname) public {
+        User storage user = users[_userName];
+        
+        require(!user.exist, "ERROR: User already exists!");
+        
+        users[_userName] = User({
+            id: msg.sender,
+            name: _userName,
+            dname: _userDname,
+            exist: true
+        });
+    }
+}
